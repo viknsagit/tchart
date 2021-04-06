d034a4e08994def9536aea6df4d0a4c9d4c0813c
viknsagit <viknsagitarius@gmail.com>
2021-04-06 18:29:16 +0500

    Интерфейс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,7 +42,7 @@ namespace tchart
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Location = new System.Drawing.Point(86, 30);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
             series1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.BackwardDiagonal;
             series1.ChartArea = "ChartArea1";
@@ -55,7 +55,7 @@ namespace tchart
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(86, 415);
+            this.button1.Location = new System.Drawing.Point(12, 402);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -66,7 +66,7 @@ namespace tchart
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 384);
+            this.label1.Location = new System.Drawing.Point(12, 377);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 2;
@@ -74,7 +74,7 @@ namespace tchart
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(191, 415);
+            this.button2.Location = new System.Drawing.Point(103, 402);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -86,7 +86,7 @@ namespace tchart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(501, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
